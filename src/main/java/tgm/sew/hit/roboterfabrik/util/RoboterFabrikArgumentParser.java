4035669bd83db4f57bcce3d400406c05146195b9
Burkhard Hampl <bhampl@student.tgm.ac.at>
2014-09-27 12:04:20 +0200

    Finished RoboterFabrikArgumentParser

--- src/main/java/tgm/sew/hit/roboterfabrik/util/RoboterFabrikArgumentParser.java
+++ src/main/java/tgm/sew/hit/roboterfabrik/util/RoboterFabrikArgumentParser.java
@@ -1,5 +1,8 @@
 package tgm.sew.hit.roboterfabrik.util;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.apache.commons.cli.BasicParser;
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.CommandLineParser;
@@ -8,28 +11,56 @@ import org.apache.commons.cli.OptionBuilder;
 import org.apache.commons.cli.Options;
 import org.apache.commons.cli.ParseException;
 
+/**
+ * Ein Argumenten Parser fuer die RoboterFabrik
+ * 
+ * @author Burkhard Hampl
+ */
 public class RoboterFabrikArgumentParser {
-	
+
+	/**
+	 * Parst die angegebenen argumente in eine {@link Map}
+	 * 
+	 * @param args
+	 *            argumente, die geparst werde soll
+	 * @return eine {@link Map}, die alle für uns notwendige Parameter
+	 *         zurückgibt
+	 */
 	@SuppressWarnings("static-access")
-	public static CommandLine parseArgs(String[] args) {
+	public static Map<String, String> parseArgs(String[] args) {
+		Map<String, String> pars = new HashMap<String, String>();
 		Options options = new Options();
-		Option logDir = OptionBuilder.withArgName("-logs").withDescription("Das Verzeichnis in welches die Logs gespeichert werden").hasArg().create(); //new Option("-logs", "Das Verzeichnis in welches die Logs gespeichert werden");
-		Option anzahlLieferanten = OptionBuilder.withArgName("-lieferanten").withDescription("Anzahl an Lieferanten die Teile ans Lager liefern").hasArg().create(); //new Option("-lieferanten", "Anzahl an Lieferanten die Teile ans Lager liefern");
-		Option anazhlMonteure = OptionBuilder.withArgName("-monteure").withDescription("Anzahl der Menteure die gleichzeitig Threadees bauen").hasArg().create(); //new Option("-monteure", "Anzahl der Monteure die gleichzeitig Threadees bauen");
-		Option dauerLaufzeit = OptionBuilder.withArgName("-laufzeit").withDescription("Dauer der Laufzeit").hasArg().create(); //new Option("-laufzeit", "Dauer der Laufzeit");
+
+		// Defenieren der notwendigen Parameter
+		Option lagerdir = OptionBuilder.isRequired().withLongOpt("lager").withDescription("Das Verzeichnis in welches das Lager gespeichert wird").hasArg().create();
+		Option logDir = OptionBuilder.isRequired().withLongOpt("logs").withDescription("Das Verzeichnis in welches die Logs gespeichert werden").hasArg().create();
+		Option anzahlLieferanten = OptionBuilder.isRequired().withLongOpt("lieferanten").withDescription("Anzahl an Lieferanten die Teile ans Lager liefern").hasArg().create();
+		Option anazhlMonteure = OptionBuilder.isRequired().withLongOpt("monteure").withDescription("nzahl der Menteure die gleichzeitig Threadees bauen").hasArg().create();
+		Option dauerLaufzeit = OptionBuilder.isRequired().withLongOpt("laufzeit").withDescription("Dauer der Laufzeit").hasArg().create();
+
+		// Hinzufuegen zu den Options
+		options.addOption(lagerdir);
 		options.addOption(logDir);
 		options.addOption(anzahlLieferanten);
 		options.addOption(anazhlMonteure);
 		options.addOption(dauerLaufzeit);
-		
+
 		CommandLineParser parser = new BasicParser();
 		CommandLine line = null;
-	    try {
+		try {
+			// parsen der Argumente
 			line = parser.parse(options, args);
+
+			// zur Hashmap hinzufuegen
+			pars.put("lager", line.getOptionValue("lager"));
+			pars.put("logs", line.getOptionValue("logs"));
+			pars.put("lieferanten", line.getOptionValue("lieferanten"));
+			pars.put("monteure", line.getOptionValue("monteure"));
+			pars.put("laufzeit", line.getOptionValue("laufzeit"));
 		} catch (ParseException e) {
 			System.err.println("Error while parsing: " + e.getMessage());
 		}
-	    
-	    return line;
+
+		return pars;
 	}
 }
