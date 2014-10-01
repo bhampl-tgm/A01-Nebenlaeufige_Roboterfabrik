4f4fd38562319c094393f93eacc86cfdf6b274fa
tgm-sgeyer <sgeyer@student.tgm.ac.at>
2014-10-01 21:35:21 +0200

    Modified simulation

--- src/main/java/tgm/sew/hit/roboterfabrik/Simulation.java
+++ src/main/java/tgm/sew/hit/roboterfabrik/Simulation.java
@@ -1,7 +1,6 @@
 package tgm.sew.hit.roboterfabrik;
 
 import java.util.Map;
-import java.util.Properties;
 
 import org.apache.commons.cli.ParseException;
 
@@ -22,9 +21,8 @@ public class Simulation {
 	public static void main(String[] args) {
 		try {
 			Map<String, String> param = RoboterFabrikArgumentParser.parseArgs(args);
-			Properties props = System.getProperties();
-			props.setProperty("sys.log", ""+param.get("logs"));
-			Sekretariat sk = new Sekretariat(Integer.parseInt(param.get("monteure")), Integer.parseInt(param.get("lieferanten")), param.get("lager"),Integer.parseInt(param.get("laufzeit")));
+			System.setProperty("sys.log", "" + param.get("logs"));
+			Sekretariat sk = new Sekretariat(Integer.parseInt(param.get("monteure")), Integer.parseInt(param.get("lieferanten")), param.get("lager"), Integer.parseInt(param.get("laufzeit")));
 		} catch (ParseException e) {
 			System.err.println("Error while parsing: " + e.getMessage());
 			RoboterFabrikArgumentParser.printHalp();
