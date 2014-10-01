6ae01bbf08b2e7cd2f413551f0e2d7eeb1796de8
Burkhard Hampl <bhampl@student.tgm.ac.at>
2014-10-01 18:12:49 +0200

    Added Lieferanten to Sekretariat

--- src/main/java/tgm/sew/hit/roboterfabrik/Simulation.java
+++ src/main/java/tgm/sew/hit/roboterfabrik/Simulation.java
@@ -24,13 +24,13 @@ public class Simulation {
 			Map<String, String> param = RoboterFabrikArgumentParser.parseArgs(args);
 			Properties props = System.getProperties();
 			props.setProperty("sys.log", ""+param.get("logs"));
-			Sekretariat sk = new Sekretariat(Integer.parseInt(param.get("monteure")), param.get("lager"));
+			Sekretariat sk = new Sekretariat(Integer.parseInt(param.get("monteure")), Integer.parseInt(param.get("lieferanten")), param.get("lager"));
 		} catch (ParseException e) {
 			System.err.println("Error while parsing: " + e.getMessage());
 			RoboterFabrikArgumentParser.printHalp();
 			System.exit(1);
 		} catch (NumberFormatException e) {
-			System.err.println("Error while parsing: montuere must be int");
+			System.err.println("Error while parsing: montuere and lieferanten must be int");
 			RoboterFabrikArgumentParser.printHalp();
 			System.exit(1);
 		} catch (Exception e) {
